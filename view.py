fb3060ec3b09da3eb5f3016f2e8642de89a061e7
Ivry Frost <ifrost@olin.edu>
2025-04-25 11:49:42 -0400

    one more Ivry Commit

--- view.py
+++ view.py
@@ -5,28 +5,13 @@ Contains the View class
 
 class View:
     """
-    Has all of the view thingys
+    Contains every method used to display
     """
 
     def __init__(self):
         """
-        you say blah blah blah blah!
+        Initializes an instance of the view class.
         """
-
-    def ask_hit_or_stay(self):
-        """
-        I do not say blah blah blah blah!
-        """
-        try:
-            action = input("Would you like to hit or stay?\n(h/s)")
-            if action == "h" or action == "H":
-                return True
-            elif action == "s" or action == "S":
-                return False
-            raise KeyError
-        except KeyError:
-            print("what the shit my guy")
-            self.ask_hit_or_stay()
 
     def show_cards(self):
         """
